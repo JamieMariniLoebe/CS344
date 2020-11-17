35753f9c57ca024a6b2c7aed7dd6d93933183d9e
Jamie Loebe <loebej@os1.engr.oregonstate.edu>
2020-11-16 22:28:36 -0800

    Final files, fixed bugs

--- Multi_Thread_HW4/main.c
+++ Multi_Thread_HW4/main.c
@@ -1,47 +1,50 @@
+/**************************************************
+* Name: Jamie Loebe
+* Course: CS344
+* Date: 11-15-20
+* Description: Multi-threaded program that uses 4
+* threads in a producer-consumer approach
+**************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <math.h> // must link with -lm
+#include <math.h>
 
 // Size of the buffers, hold no more than 50 lines of 1000 chars
 #define MAX_LINES 50
 #define CHARS 1000 //Account for null terminator
 #define CHUNKSIZE 80
 
-/* Buffer 1, shared resource between input thread and line separator thread. */
+/* Buffers, shared resource between threads. */
 char buffer_1[MAX_LINES*CHARS]; 
+char buffer_2[MAX_LINES*CHARS]; 
+char buffer_3[MAX_LINES*CHARS]; 
 
+/* Pointers to buffers */
 char* input_thread_ptr = buffer_1; 
-char* newLineProc = buffer_1;
-char* plusSignProc = buffer_1;
+char* newLineProc = buffer_2;
+char* plusSignProc = buffer_3;
 
-
-
-int prod_idx = 0;
-int con_idx = 0;
-int stopCond = 0;
-int plusSignCount = 0;
-int fullBuf = 0;
+/* Flags to track buffer */
+int buffer1_stopped = 0;
+int buffer2_stopped = 0;
+int buffer3_stopped = 0;
 
 //Initialize the mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 //Initialize condiiton variables
-pthread_cond_t inputReceived = PTHREAD_COND_INITIALIZER;
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t buffer1Updated = PTHREAD_COND_INITIALIZER;
+pthread_cond_t buffer2Updated = PTHREAD_COND_INITIALIZER;
+pthread_cond_t buffer3Updated = PTHREAD_COND_INITIALIZER;
 pthread_cond_t stopProc = PTHREAD_COND_INITIALIZER;
-pthread_cond_t outputNow = PTHREAD_COND_INITIALIZER;
-
 
 /*Reads in lines of characters from the standard input*/
 void* getInput(void* args){
-    /*-->Read in lines of chars from user input )
-    -->Lines of input: A sequence of the allowed characters ASCII 32-126 that does NOT include a line separator.
-    -->Followed by a line separator (\n). 
-    -->Process lines until encounter input line with only 'STOP' chars followed immediately by a line spearator */
 
     char* stop = "STOP\n"; //When to stop reading input
     size_t len = sizeof(buffer_1)/sizeof(buffer_1[0]); 
@@ -50,139 +53,127 @@ void* getInput(void* args){
     int lineNum = 0;
 
     /*Read entire line from stdin, store in buffer_1*/
-
-    while(stopCond == 0) 
+    while(buffer1_stopped == 0) 
     {
-        //printf("%d\n", input_thread_ptr);
         lineSize = getline(&input_thread_ptr, &len, stdin);
 
         pthread_mutex_lock(&mutex);
-        //printf("%d, %d, %d: %s\n", len, stopCond, lineSize, input_thread_ptr);
-        if(lineSize >= 80) {
-            fullBuf = 1;
-        }
-
         if(strcmp(input_thread_ptr, stop) == 0) {
-            //printf("Stop: %d\n", stopCond);
-            stopCond = 1;
-            //printf("Stop: %d\n", stopCond);
+            buffer1_stopped = 1;
         }
         else 
         {
             input_thread_ptr += lineSize; //Move to next idx
             len -= lineSize; //Decreasing buffer size
         }
+        /*Signal that buffer1 has been updated with data*/
+        pthread_cond_signal(&buffer1Updated);
         pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&inputReceived);
     }
-    pthread_cond_signal(&stopProc); //Signal to stop processing reached and begin outputting data
+    pthread_mutex_lock(&mutex);
+    /* Signal to stop process*/
+    pthread_cond_signal(&stopProc);
+    pthread_mutex_unlock(&mutex);
+
     return NULL;
 }
 
 /*Replaces every line separator in the input by a space*/
-void* replaceNewLines(void* args){
-    int stop = 0;
+void* replaceNewLines(void* args) {
+    char* sourceptr = buffer_1;
 
-    pthread_mutex_lock(&mutex);
-    pthread_cond_wait(&inputReceived, &mutex); //wait for input 
+    /*Wait for buffer1 data and then process*/
+    while(!buffer2_stopped) {
+        pthread_mutex_lock(&mutex);
+        if(!buffer1_stopped) {
+            pthread_cond_wait(&buffer1Updated, &mutex); //wait for input 
+        }
 
-    while(!stop) {
-        
-        // Process up to 80 chars and only while newLineProc < input_thread_ptr
-        for(int i=0; i<CHUNKSIZE && newLineProc < input_thread_ptr; i++) {
-            if(*newLineProc == '\n') {
+        /*Process up to 80 chars, change '\n' to space*/
+        for(int i=0; i<CHUNKSIZE && sourceptr < input_thread_ptr; i++) {
+            if(*sourceptr == '\n') {
                 *newLineProc = ' ';
             }
+            else {
+                *newLineProc = *sourceptr;
+            }
+            sourceptr++;
             newLineProc++;
         }
-        if(stopCond == 1 && newLineProc == input_thread_ptr) {
-            stop = 1;
+        if(buffer1_stopped == 1 && sourceptr == input_thread_ptr) {
+            buffer2_stopped = 1;
         }
+        pthread_cond_signal(&buffer2Updated); //signal buffer2 has data
         pthread_mutex_unlock(&mutex);
     }
-
     return NULL;
 }
 
 /*Replaces every pair of plus signs with '^' i.e., "++" replace with a "^"*/
 void* replacePlusSigns(void* args){
-    int stop = 0;
+    char* sourceptr = buffer_2;
 
-    pthread_mutex_lock(&mutex);
-    pthread_cond_wait(&inputReceived, &mutex); //wait for input 
+    while(!buffer3_stopped) {
+        pthread_mutex_lock(&mutex);
+        if(!buffer2_stopped) {
+            pthread_cond_wait(&buffer2Updated, &mutex); //wait for data from buffer2
+        }
 
-
-    while(!stop) {
-
-        //printf("%d == %d, %d\n", plusSignProc, input_thread_ptr, stopCond);
-
-        // Process up to 80 chars and only while plusSignProc < input_thread_ptr
-        for(int i=0; i<CHUNKSIZE && plusSignProc < input_thread_ptr; i++) {
-            if(strncmp(plusSignProc, "++", 2) == 0) { //Look for 2 plus signs
+        // Process up to 80 chars
+        for(int i=0; i<CHUNKSIZE && sourceptr < newLineProc; i++) {
+            if(strncmp(sourceptr, "++", 2) == 0) { //Look for 2 plus signs
                 *plusSignProc = '^';
-
-                plusSignCount++;
-
-                char* ptr = plusSignProc + 1;
-
-                while(ptr < input_thread_ptr) {
-                    *ptr = *(ptr+1);
-                    ptr++;
-                }
-                input_thread_ptr--; //Shifting input_thread_ptr by 1 to accomoadate for removal of 2nd +
+                sourceptr++; //moving past 2nd +
+                i++;
+            }
+            else {
+                *plusSignProc = *sourceptr;
             }
             plusSignProc++;
+            sourceptr++;
         }
-        if(stopCond == 1 && plusSignProc >= input_thread_ptr) {
-            stop = 1;
+        if(buffer2_stopped == 1 && sourceptr == newLineProc) {
+            buffer3_stopped = 1;
         }
-        pthread_cond_signal(&outputNow);
+        pthread_cond_signal(&buffer3Updated);
         pthread_mutex_unlock(&mutex);
-
     }
     return NULL;
 }
 
 /*Write this processed data to standard output as lines of exactly 80 characters*/
 void* outputText(void* args){
-    char* outputPtr = buffer_1;
-    char finalBuf[82] = { 0 };
+    char* outputPtr = buffer_3;
     int i = 0;
-    int size = 80;
+    int totalSize = 0;
 
-    pthread_mutex_lock(&mutex);
-    //printf("\n=======Break=======\n");
-    //printf("FULLBUF: %d", fullBuf);
-
-    /*while(fullBuf) {
-        for(int i=0; i<81; i++) {
-
+    /*Wait until buffer1 done*/
+    do {
+        pthread_mutex_lock(&mutex);
+        if(buffer1_stopped) {
+            break;
         }
-    }*/
-
-    printf("input: %d, output: %d\n", input_thread_ptr, outputPtr);
-
-    pthread_cond_wait(&outputNow, &mutex);
-
-    while(outputPtr < input_thread_ptr) { //
-        printf("\n========BREAK========\n");
-        memset(finalBuf, '\0', sizeof(finalBuf));
-        
-        //printf("SIZE: %d\n", size);
-
-        if((int)(input_thread_ptr - outputPtr) < 80) { //Checking buffer size
-            size = (int)(input_thread_ptr - outputPtr);
-            printf("SIZE: %d\n", size);
-        } 
-
-
-        memcpy(finalBuf, outputPtr, size);
-        outputPtr += size;
-        printf("SIZE: %d\n", size);
-        printf("%s", finalBuf);
         pthread_mutex_unlock(&mutex);
+    } while(1);
+
+    /* While still data to process */
+    while(outputPtr < plusSignProc || !buffer3_stopped) {
+        pthread_mutex_unlock(&mutex);
+        pthread_mutex_lock(&mutex);
+
+        printf("%c", *outputPtr++); //print each char
+        if(++totalSize % 80 == 0) { //Check if divisible by 80
+            printf("\n");
+        }
+
+        pthread_mutex_unlock(&mutex);
+        pthread_mutex_lock(&mutex);
     }
-    printf("Ending program\n\n");
+    pthread_mutex_unlock(&mutex);
+
+    if(++totalSize % 80 != 1) {
+        printf("\n"); //add newline character
+    }
     return NULL;
 }
 
@@ -202,14 +193,6 @@ int main(void) {
     pthread_join(newLines, NULL);
     pthread_join(plusSigns, NULL);
     pthread_join(output, NULL); 
-
-    /*getInput();
-
-    replaceNewLines();
-
-    replacePlusSigns();
-
-    outputText();*/
     
     return 0;
 }
